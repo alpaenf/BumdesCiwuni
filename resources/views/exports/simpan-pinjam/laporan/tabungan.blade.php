dd5c3c9499e26b6e7df189b00374de49e5ce979e
alpaenf <alfaenf_@gmail.com>
2026-06-13 15:31:22 +0700

    fix: terapkan filter jenis tabungan (reguler/sembako) pada laporan tabungan (web, pdf, excel)

--- resources/views/exports/simpan-pinjam/laporan/tabungan.blade.php
+++ resources/views/exports/simpan-pinjam/laporan/tabungan.blade.php
@@ -1,7 +1,7 @@
 @php
     use App\Models\Tabungan;
     $headerColor = '#1B5E20';
-    $jenisLabel  = 'Tabungan';
+    $jenisLabel  = 'Tabungan ' . ucfirst($jenis ?? 'reguler');
     $reportTitle = 'Laporan Transaksi ' . $jenisLabel;
     $periodLabel = ($filters['start_date'] ?? $filters['end_date'])
         ? 'Periode: ' . ($filters['start_date'] ?? '...') . ' s/d ' . ($filters['end_date'] ?? 'sekarang')
